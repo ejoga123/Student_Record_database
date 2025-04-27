c738ec6e6dda5ec375d050997bb1d24a4355c001
Ejoga Emmanuel <ejogaemmanuel62@gmail.com>
2025-04-27 07:02:55 +0100

    insertion data to the tables

--- student.sql
+++ student.sql
@@ -36,6 +36,7 @@ CREATE TABLE courses (
 );
 
 
+
 -- create table for Teachers 
 CREATE TABLE teachers (
     teacher_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -112,3 +113,79 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id)
 );
+
+-- data insertion for departments
+INSERT INTO departments (description) 
+VALUES
+('Department of Computer Science'),
+('Department of Mathematics'),
+('Department of Physics'),
+('Department of Mechanical Engineering'),
+('Department of Electrical Engineering'),
+('Department of Civil Engineering'),
+('Department of Business Administration'),
+('Department of Economics'),
+('Department of Law'),
+('Department of Medicine'),
+('Department of Pharmacy'),
+('Department of Political Science'),
+('Department of Mass Communication'),
+('Department of Psychology'),
+('Department of Sociology'),
+('Department of Chemistry'),
+('Department of Biology'),
+('Department of Architecture'),
+('Department of Fine Arts');
+
+
+-- data insertion for teachers
+INSERT INTO teachers (first_name, last_name, email, phone, department_id, hire_date)
+VALUES 
+('Thresa', 'Omaga', 'thresaomaga@school.edu', '0700000007', 1, '2023-09-01'),
+('jame', 'Idoko', 'jamesidoko@school.edu', '0700000008', 3, '2023-06-15'),
+('James', 'Agbo', 'jamesagbo@school.edu', '070000003', 3, '2021-09-01'),
+('Mary', 'Odeh', 'maryodeh@school.edu', '0700000004', 4, '2025-01-15');
+
+
+-- data insertion for courses
+INSERT INTO courses (department_id, course_code, course_name, description, credit_hours) VALUES
+(1, 'CSC101', 'Intro to Programming', 'Basics of Python', 3),
+(2, 'MAT101', 'Calculus I', 'Differential Calculus', 4),
+(3, 'PHY101', 'General Physics', 'Mechanics and Thermodynamics', 4);
+
+
+-- data insertion for students
+INSERT INTO students (first_name, last_name, date_of_birth, gender, email, phone, address) VALUES
+('Alice', 'Johnson', '2003-05-12', 'Female', 'alice@example.com', '0701234567', 'Lagos'),
+('Mercy', 'Adikwu', '2002-09-23', 'Male', 'mercy@example.com', '0709876543', 'Benue'),
+('Mary', 'Adikwu', '2002-03-22', 'Male', 'mary@example.com', '0709876553', 'Yobe'),
+('Blessing', 'Agbo', '2001-09-23', 'Female', 'blessing@example.com', '0709875543', 'Nassrawa'),
+('Victoria', 'Odeh', '2006-09-23', 'Male', 'victor@example.com', '0709976543', 'Dlta'),
+('Agnes', 'Ejoga', '2004-09-23', 'Female', 'agnes@example.com', '0709878543', 'Eke'),
+('Easter', 'Sunday', '2002-09-13', 'Female', 'easter@example.com', '0709876541', 'Adoka'),
+('Emmanuel', 'Sunday', '2001-09-23', 'Male', 'emmanuel@example.com', '0709876943', 'Ajide'),
+('Frankly', 'Ejeh', '2002-09-24', 'Male', 'Frankly@example.com', '0809876540', 'apakli'),
+('Philp', 'Ohene', '2005-09-23', 'Male', 'philip@example.com', '0709876548', 'Aokpasu');
+
+
+-- data insertion for enrollments
+INSERT INTO enrollments (student_id, course_id, semester, year)
+VALUES
+(11, 1, 'Fall', 2024),
+(12, 2, 'Fall', 2024),
+(13, 1, 'Fall', 2024),
+(14, 3, 'Fall', 2024);
+
+-- data insertion for grades
+INSERT INTO grades (student_id, course_id, semester, year, grade, remarks) 
+VALUES
+(11, 1, 'Fall', 2024, 'A', 'Excellent'),
+(12, 2, 'Fall', 2024, 'B+', 'Good'),
+(13, 1, 'Fall', 2024, 'B', 'Good'),
+(14, 3, 'Fall', 2024, 'C+', 'Average');
+
+--  data insertion for fees
+INSERT INTO fees (student_id, amount, payment_date, status, payment_method, transaction_reference)
+VALUES
+(11, 500.00, '2024-09-15', 'Paid', 'Card', 'TRX10001'),
+(12, 500.00, '2024-09-20', 'Paid', 'Mobile Money', 'TRX10002');
